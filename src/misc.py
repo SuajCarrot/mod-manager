043473d2f847ff39d173cb243774c88ed3a5b91f
SuajCarrot <suaj9@protonmail.com>
2021-06-02 09:04:35 -0500

    Added license to all modules, improved README and translated it to spanish

--- src/misc.py
+++ src/misc.py
@@ -1,3 +1,21 @@
+# The module that defines general-purpose functions.
+#
+# Copyright (C) 2021  Suaj
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+
 from os.path import expanduser as home
 from os.path import isdir as is_dir
 from os.path import join as path_join
